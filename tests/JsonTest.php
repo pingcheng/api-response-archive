b5d7d6cb4bfa32346b3a947cf02fe4982330484a
Ping Cheng <ping.cheng@eduplus.net.au>
2018-03-31 00:52:37 +1100

    remove ? in test file

--- tests/JsonTest.php
+++ tests/JsonTest.php
@@ -13,7 +13,7 @@ class JsonTest extends TestCase
         ]
     ];
 
-    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    public function __construct(string $name = null, array $data = [], string $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
         ApiResponse::send_header(false);
